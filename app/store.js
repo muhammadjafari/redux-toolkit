3efb44ff3181cef4fa76dd9c98a513f0e2b3b367
Mohammad Jafari <jafariimuhammad@gmail.com>
2023-10-27 14:57:21 +0330

    user reducer with async thunk

--- app/store.js
+++ app/store.js
@@ -1,16 +1,18 @@
 const configureStore = require("@reduxjs/toolkit").configureStore
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice")
-const reducLogger = require('redux-logger')
+const userReducer = require("../features/user/userSlice")
+// const reducLogger = require('redux-logger')
 
-const logger = reducLogger.createLogger()
+// const logger = reducLogger.createLogger()
 
 const store = configureStore({
     reducer:{
         cake: cakeReducer,
-        icecream: icecreamReducer
+        icecream: icecreamReducer,
+        user: userReducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
 module.exports = store ;
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const store = require("./app/store.js");
 const cakeActions = require("./features/cake/cakeSlice.js").cakeActions
+const fetchUsers = require("./features/user/userSlice.js").fetchUsers
 
 console.log("initial state",store.getState());
 
-// unsubscribe = store.subscribe(() => {
-//     console.log(store.getState())
-// })
+unsubscribe = store.subscribe(() => {
+    console.log("updated state",store.getState())
+})
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3))
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3))
+
+store.dispatch(fetchUsers());
 
 // unsubscribe();
