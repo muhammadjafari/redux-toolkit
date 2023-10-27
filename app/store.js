edd6f7a6b3e376923b97201330fb6ccd4702fdd9
Mohammad Jafari <jafariimuhammad@gmail.com>
2023-10-27 11:48:14 +0330

    logger added

--- app/store.js
+++ app/store.js
@@ -1,12 +1,16 @@
 const configureStore = require("@reduxjs/toolkit").configureStore
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice")
+const reducLogger = require('redux-logger')
+
+const logger = reducLogger.createLogger()
 
 const store = configureStore({
     reducer:{
         cake: cakeReducer,
         icecream: icecreamReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
 module.exports = store ;
--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const cakeActions = require("./features/cake/cakeSlice.js").cakeActions
 
 console.log("initial state",store.getState());
 
-unsubscribe = store.subscribe(() => {
-    console.log(store.getState())
-})
+// unsubscribe = store.subscribe(() => {
+//     console.log(store.getState())
+// })
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3))
 
-unsubscribe();
+// unsubscribe();
